c03983e4ba1cf8319122975c64ae32a27e620b4d
Rob Saunders <rob@wearebrandnew.com>
2016-06-02 14:53:22 +0800

    Added initial project files.

---
+++ TemplateWithoutStoryboards/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TemplateWithoutStoryboards
+//
+//  Created by Rob Saunders on 2/06/2016.
+//  Copyright © 2016 We Are Brand New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
